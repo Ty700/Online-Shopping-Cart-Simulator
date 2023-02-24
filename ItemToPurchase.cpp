99bc5298629de2114d93685d1938c63eca03941f
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-02-23 18:29:11 -0600

    Create ItemToPurchase.cpp

---
+++ ItemToPurchase.cpp
@@ -0,0 +1,35 @@
+#include "ItemToPurchase.h"
+
+ItemToPurchase::ItemToPurchase() {
+    itemName = "none";
+    itemPrice = 0;
+    itemQuantity = 0;
+}
+
+void ItemToPurchase::SetName(string name) {
+    itemName = name;
+}
+
+string ItemToPurchase::GetName() const {
+    return itemName;
+}
+
+void ItemToPurchase::SetPrice(int price) {
+    itemPrice = price;
+}
+
+int ItemToPurchase::GetPrice() const {
+    return itemPrice;
+}
+
+void ItemToPurchase::SetQuantity(int quantity) {
+    itemQuantity = quantity;
+}
+
+int ItemToPurchase::GetQuantity() const {
+    return itemQuantity;
+}
+
+int ItemToPurchase::GetTotal(){
+   return itemQuantity * itemPrice;
+   }
