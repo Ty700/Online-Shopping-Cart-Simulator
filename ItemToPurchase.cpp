66c578ddbd96d29731c62be75e8b2e53234136fa
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-03-01 17:00:53 -0600

    Changed a few things

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,35 +1,35 @@
 #include "ItemToPurchase.h"
+#include <iostream>
+#include <iomanip>
+ItemToPurchase::ItemToPurchase() : itemName("None"), itemQuantity(0), itemPrice(0) {} //Default Constructor
 
-ItemToPurchase::ItemToPurchase() {
-    itemName = "none";
-    itemPrice = 0;
-    itemQuantity = 0;
-}
-
-void ItemToPurchase::SetName(string name) {
-    itemName = name;
-}
-
-string ItemToPurchase::GetName() const {
+//Getters 
+std::string ItemToPurchase::GetName(){ //Returns item name
     return itemName;
 }
 
-void ItemToPurchase::SetPrice(int price) {
-    itemPrice = price;
-}
-
-int ItemToPurchase::GetPrice() const {
+double ItemToPurchase::GetPrice(){ //Returns item Price
     return itemPrice;
 }
 
-void ItemToPurchase::SetQuantity(int quantity) {
-    itemQuantity = quantity;
-}
-
-int ItemToPurchase::GetQuantity() const {
+int ItemToPurchase::GetQuantity(){ //Returns item Quantity
     return itemQuantity;
 }
 
-int ItemToPurchase::GetTotal(){
-   return itemQuantity * itemPrice;
-   }
+//Setters
+inline void ItemToPurchase::SetName(std::string& itemName){ this->itemName = itemName; } //Name
+
+inline void ItemToPurchase::SetPrice(double& price){ this->itemPrice = price; } //Price
+
+inline void ItemToPurchase::SetQuantity(int& quantity){ this->itemQuantity = quantity; } //Quantity
+
+//Uncle Sam's Piece of the pie
+double tax(double price){
+    return (price += price*0.0825);
+}
+
+//Print
+void ItemToPurchase::Print(){
+    std::cout << GetName() << " " << GetQuantity() << " @ $" << GetPrice() << " = $" << std::fixed << std::setprecision(2) << tax(GetPrice() * GetQuantity()) << std::endl;
+}
+
