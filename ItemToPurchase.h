4e9834f9045aa058c69849fc6e95a1a953e84391
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-03-01 17:01:06 -0600

    changed a few things

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,23 +1,21 @@
 #ifndef ITEM_TO_PURCHASE_H
 #define ITEM_TO_PURCHASE_H
-
 #include <string>
-using namespace std;
 
-class ItemToPurchase {
+class ItemToPurchase { 
 public:
-    ItemToPurchase(); // default constructor
-    void SetName(string name);
-    string GetName() const;
-    void SetPrice(int price);
-    int GetPrice() const;
-    void SetQuantity(int quantity);
-    int GetQuantity() const;
-    int GetTotal();
+    ItemToPurchase(); //Default Constructor
+    void SetName(std::string& itemName);
+        std::string GetName();
+    void SetPrice(double& itemPrice);
+        double GetPrice();
+    void SetQuantity(int& itemQuantity);
+        int GetQuantity();
+    void Print();
 private:
-    string itemName;
-    int itemPrice;
-    int itemQuantity;
+        std::string itemName;
+        double itemPrice;
+        int itemQuantity;
 };
 
 #endif
